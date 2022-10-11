3006a3b1e1dd17f72d393010a631380541bd1d99
Kaitrye <93823684+Kaitrye@users.noreply.github.com>
2022-10-11 12:12:02 +0300

    Update Task2.cpp

--- Task2.cpp
+++ Task2.cpp
@@ -5,15 +5,12 @@ int main() {
 int n;
 std::cin >> n;
 
-for (int row=1, star=1, space=n-1; row<=n; ++row, star+=2, --space) {
-	for (int i=1; i<=space; ++i) {
+for (int row=1; row<=n; ++row) {
+	for (int i=1; i<=n-row; ++i) {
 		std::cout << ' ';
 	}
-	for (int i=1; i<=star; ++i){
+	for (int i=1; i<=2*row-1; ++i){
 		std::cout << '*';
-	}
-	for (int i=1; i<=space; ++i) {
-		std::cout << ' ';
 	}
 	std::cout << std::endl;
 }
