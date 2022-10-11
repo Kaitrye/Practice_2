3bad969e691de3f4ff4d58c9cf53857486f03d11
Kaitrye <st097427@student.spbu.ru>
2022-10-11 11:42:42 +0300

    Update files

--- Task3.cpp
+++ Task3.cpp
@@ -12,20 +12,20 @@ int n;
 std::cin >> n;
 
 for (int row=1, star=1, space=n-1; row<=n; ++row, star+=2, --space) {
-	Space(z);
+	Space(space);
 	for (int i=1; i<=star; ++i){
 		std::cout << '*';
 	}
-	Space(z);
+	Space(space);
 	std::cout << std::endl;
 }
 
 for (int row=2, star=1+(n-2)*2, space=1; row<=n; ++row, star-=2, ++space) {
-	Space(z);
+	Space(space);
 	for (int i=1; i<=star; ++i) {
 		std::cout << '*';
 	}
-	Space(z);
+	Space(space);
 	std::cout << std::endl;
 }
 return 0;
--- Task4.cpp
+++ Task4.cpp
@@ -13,22 +13,22 @@ std::cin >> n;
 
 for (int row=1, star=1, space=n-1; row<=n; ++row, star+=2, --space) {
 	for (int count=0; count<n; ++count){
-		Space(z);
+		Space(space);
 		for (int i=1; i<=star; ++i){
 			std::cout << '*';
 		}
-		Space(z);
+		Space(space);
 	}
 	std::cout << std::endl;
 }
 
 for (int row=2, star=1+(n-2)*2, space=1; row<=n; ++row, star-=2, ++space) {
 	for (int count=0; count<n; ++count) {
-		Space(z);
+		Space(space);
 		for (int i=1; i<=star; ++i) {
 			std::cout << '*';
 		}
-		Space(z);
+		Space(space);
 	}
 	std::cout << std::endl;
 }
